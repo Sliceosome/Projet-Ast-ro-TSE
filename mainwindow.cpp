a4888f6f8f441ed873b9f9337beff0f516ca79dc
Kevin Lelièvre <kevin@wizekat.eu>
2022-04-15 16:30:11 +0200

    suite
    suite du commit précédent

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -16,7 +16,6 @@ void MainWindow::update_all(){
     QPixmap *pixmap = new QPixmap(camera_->Mat2QPixmap(camera_->GetFrame()));
     //*pixmap = pixmap->scaled(cv::CAP_PROP_FRAME_WIDTH, (int)((double)cv::CAP_PROP_FRAME_HEIGHT*(2/3)),Qt::KeepAspectRatio, Qt::FastTransformation);
     ui->label->setPixmap(*pixmap);
-
 };
 
 
